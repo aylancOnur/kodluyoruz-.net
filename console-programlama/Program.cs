6b22c97bd8c9e6eff586f3778d98bd08a2231044
Fr3shBrain <aylanc.onur@gmail.com>
2021-12-10 18:23:10 +0300

    Console Programlama

---
+++ console-programlama/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace console_programlama
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("İsminizi Giriniz:");
+            string name = Console.ReadLine();
+            Console.WriteLine("Soyadınızı Giriniz:");
+            string lastname = Console.ReadLine();
+            Console.WriteLine("Merhaba " + name + " " + lastname);
+        }
+    }
+}
